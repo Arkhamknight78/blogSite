e0b8083d91fc48ab57c9e02b83cf0481d55126ea
AK7824 <vinsxn02@gmail.com>
2025-05-15 19:20:20 +0530

    conflict resolution

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,13 +5,6 @@ function Logo({width= '50px'}) {
     <div>
       <img
         src="/blog_logo1.png"
-// =======
-// function Logo({width= '100px'}) {
-//   return (
-//     <div>
-//       <img
-//         src="https://www.svgrepo.com/show/1191/cooking.svg"
-// >>>>>>> ec8816fca8f9400fe878081c8249adb5c49b70f8
         alt="Logo"
         width={width}
     />
